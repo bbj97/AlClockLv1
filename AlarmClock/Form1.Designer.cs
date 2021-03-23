33c76b81bff46ef6af368b1ffcf90109c31edb84
blj97 <korisnickabljeonce@gmail.com>
2021-03-23 01:04:43 +0100

    m

---
+++ AlarmClock/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace AlarmClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTime = new System.Windows.Forms.Label();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.btnPostavi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(158, 37);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(55, 17);
+            this.lblTime.TabIndex = 2;
+            this.lblTime.Text = "Vrijeme";
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker.Location = new System.Drawing.Point(113, 94);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(156, 22);
+            this.dateTimePicker.TabIndex = 5;
+            // 
+            // btnPostavi
+            // 
+            this.btnPostavi.Location = new System.Drawing.Point(113, 139);
+            this.btnPostavi.Name = "btnPostavi";
+            this.btnPostavi.Size = new System.Drawing.Size(156, 23);
+            this.btnPostavi.TabIndex = 0;
+            this.btnPostavi.Text = "Status";
+            this.btnPostavi.UseVisualStyleBackColor = true;
+            this.btnPostavi.Click += new System.EventHandler(this.btnPostavi_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 203);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.btnPostavi);
+            this.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.Name = "Form1";
+            this.Text = "AlarmClock";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.Button btnPostavi;
+    }
+}
+
---
+++ AlarmClock/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlarmClock
+{
+    public partial class Form1 : Form
+    {
+        private System.Timers.Timer t;
+        public Form1()
+        {
+            InitializeComponent();
+            t = new System.Timers.Timer(1000);
+            t.Start();
+            t.Elapsed += new System.Timers.ElapsedEventHandler(vrijeme);
+            t.Elapsed += Timer_Elapsed;
+        }
+
+        private void btnPostavi_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void vrijeme(object s, EventArgs e)
+        {
+            //Metoda Invoke izvršava delegata na niti koja je vlasnik
+            //rukovatelja kontrola (uobičajeno, glavna nit)
+            //MethodInvoker je delegat koji može izvršiti bilo koju
+            //metodu koja ne vraća ništa i nema parametre
+            Invoke((MethodInvoker)delegate //Anonimna metoda
+            {
+                lblTime.Text = DateTime.Now.ToLongTimeString();
+            });
+        }
+        
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            DateTime currentTime = DateTime.Now;
+            DateTime userTime = dateTimePicker.Value;
+            if (currentTime.Hour == userTime.Hour && currentTime.Minute == userTime.Minute && currentTime.Second == userTime.Second)
+            {
+                t.Stop();
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer("C:/Users/Bljeona/Documents/beepOne.wav");
+                player.Play();
+                btnPostavi.Text = "Zaustavi";
+               
+            }
+        }
+    }
+}
